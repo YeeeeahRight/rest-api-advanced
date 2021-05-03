771291c495c9e37a2568b661c43c057082423598
YeeeeahRight <yeeeeeah.right@gmail.com>
2021-05-03 20:58:22 +0300

    Add user DTO with validator

---
+++ service/src/main/java/com/epam/esm/service/validator/UserValidator.java
@@ -0,0 +1,21 @@
+package com.epam.esm.service.validator;
+
+import com.epam.esm.service.dto.UserDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserValidator implements Validator<UserDto> {
+    private static final int NAME_MAX_LENGTH = 60;
+    private static final int NAME_MIN_LENGTH = 1;
+
+    @Override
+    public boolean isValid(UserDto entity) {
+        String name = entity.getName();
+        if (name == null) {
+            return false;
+        }
+        int nameLength = name.length();
+
+        return nameLength >= NAME_MIN_LENGTH && nameLength <= NAME_MAX_LENGTH;
+    }
+}
